31e6de281226420e6983c83ee67bb8f28982ccc4
Said Agouzal <said.agouzal@telecom-paris.fr>
2025-12-01 20:57:24 +0100

    optimising newton architecture done

--- src/Newton_a1.vhd
+++ src/Newton_a1.vhd
@@ -14,7 +14,7 @@ entity newton is
 end entity ;
 
 architecture a1 of newton is 
-type T_state  is (S_WAIT,S_INT,S_COMP,S_FIN);
+type T_state  is (S_WAIT,S_COMP,S_FIN);
 signal state      : T_state;
 signal X_reg      : unsigned (2*NBITS-1 downto 0 );
 begin
@@ -25,29 +25,29 @@ begin
         if (reset='0') then
             state      <= S_WAIT ; 
             X_reg      <= to_unsigned(0,2*NBITS);
+            finished <= '0' ;
         elsif (rising_edge(clk)) then
             case( state ) is
                 when S_WAIT => if (start = '1') then 
-                                state <= S_INT ;
+                                state <= S_COMP ;
                                 end if ;
-                when S_INT  => if unsigned(A) > x"00000000FFFFFFFF" then
+                                 if unsigned(A) > x"00000000FFFFFFFF" then
                                      X_reg <= x"00000000FFFFFFFF";
                                  else
                                     X_reg <= unsigned(A);
                                 end if;    
-                               state <= S_COMP ;
                 when S_COMP => div := unsigned(A)/X_reg; 
                                X_Next := (resize(X_reg,2*NBITS + 1) + resize(div,2*NBITS + 1)) / 2;
                                if (X_reg <= X_Next(2*NBITS-1 downto 0)) then
-                                    state <= S_FIN ; 
+                                    state <= S_FIN ;
+                                    finished <=  '1' ; 
                                else
                                X_reg <= X_Next(2*NBITS-1 downto 0);
                                end if ;
-                when S_FIN  => finished <=  '1' ;
+                when S_FIN  => 
                                if (start = '0') then 
                                     finished <= '0' ;  
                                     state <= S_WAIT;
-                                    finished   <= '0' ;
                                end if ;
                 when others => null;
             end case ;
