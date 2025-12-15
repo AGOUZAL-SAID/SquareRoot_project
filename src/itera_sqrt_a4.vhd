499987fefceeed11feb9357290196d1c29e49fcf
Said Agouzal <said.agouzal@telecom-paris.fr>
2025-12-15 21:48:43 +0100

    optimisation of a4 of iterative algo done and tested successfully

--- src/itera_sqrt_a4.vhd
+++ src/itera_sqrt_a4.vhd
@@ -44,6 +44,8 @@ architecture a4 of it_sqrt is
             RES => RES
         );
     FSM : process(clk,reset) 
+		variable attache2 : std_logic;
+            
         begin
             if (reset='0') then
                 state      <= S_WAIT ; 
@@ -51,37 +53,26 @@ architecture a4 of it_sqrt is
                 Z       <= to_unsigned(0,NBITS);
                 R       <= to_signed(0,NBITS+1) ;
                 counter <= 0;
-                E1      <=  to_signed(0,NBITS+1);   
-                E2      <=  to_signed(0,NBITS+1);   
-                sub     <=   '0';
                 finished <=  '0' ;      
             elsif (rising_edge(clk)) then
             case( state ) is
             
-                when S_WAIT =>  state <= S_COMP1;
-                                D <= unsigned(A);
-                                Z <= to_unsigned(0,NBITS);
-                                R <= to_signed(0,NBITS+1) ;
-                                counter <= 0;
+                when S_WAIT =>  if (start = '1') then 
+                                state <= S_COMP1 ;
+                                end if ;
+								D <= unsigned(A);
+								counter <= 0;
+								
+                                
             
                 when S_COMP1 => 
-                                state <= S_COMP2;
-                                if (R>=to_signed(0,NBITS+1)) then
-                                    sub <= '1';
-                                    E1 <=R(NBITS+1-1-2 downto 0) & signed(D(2*NBITS-1 downto 2*NBITS-2));  
-                                    E2 <= signed(b"0" & Z(NBITS-3 downto 0) & b"01");
-                                else 
-                                    sub <= '0';
-                                    E1 <= R(NBITS+1-1-2 downto 0) & signed(D(2*NBITS-1 downto 2*NBITS-2));
-                                    E2 <= resize(signed(Z(NBITS-3 downto 0) & b"11"),NBITS+1);
-                                end if ;
-
-                when S_COMP2 => R <= signed(RES);
+                                R <= signed(RES);
                                 if (signed(RES)>=to_signed(0,NBITS+1)) then 
-                                    Z  <= Z(NBITS-2 downto 0) & '1' ;
+                                    attache2  :=  '1' ;
                                 else
-                                    Z  <= Z(NBITS-2 downto 0) & '0';
+                                    attache2  := '0';
                                 end if;
+                                Z  <= Z(NBITS-2 downto 0) & attache2 ;
                                 D <= D(2*NBITS-3 downto 0) & b"00";
                                 counter <= counter + 1;
                                 if (counter = NBITS-1) then 
@@ -91,6 +82,8 @@ architecture a4 of it_sqrt is
                                     state <= S_COMP1;
                                 end if;
                 when S_FIN   => 
+                                Z <= to_unsigned(0,NBITS);
+                                R <= to_signed(0,NBITS+1) ;
                                 if (start = '0') then 
                                     finished <= '0' ;  
                                     state <= S_WAIT;
@@ -100,4 +93,17 @@ architecture a4 of it_sqrt is
             end if ;
     end process;
     result <= std_logic_vector(Z);
+    alu_process : process( R,D,Z )
+    variable attache : unsigned(0 downto 0);
+    begin
+        E1 <= R(NBITS+1-1-2 downto 0) & signed(D(2*NBITS-1 downto 2*NBITS-2));
+        if (R(NBITS downto NBITS)=to_signed(0,1)) then
+            sub <= '1';
+            attache := b"0" ;
+        else 
+            sub <= '0';
+            attache := b"1";   
+        end if ;
+        E2 <= signed(b"0" & Z(NBITS-3 downto 0) & attache & b"1");
+    end process ; -- alu
 end a4;
